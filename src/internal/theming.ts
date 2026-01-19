80a8c545a3859646dab00c6605b1d4a267deab41
Damyan Petev <damyanpetev@users.noreply.github.com>
2026-01-19 16:34:14 +0200

    fix(theming): controller event remove on disconnect

--- src/internal/theming.ts
+++ src/internal/theming.ts
@@ -150,7 +150,7 @@ class ThemingController implements ReactiveController {
 
   /** @internal */
   public hostDisconnected(): void {
-    globalThis.addEventListener(CHANGE_THEME_EVENT, this);
+    globalThis.removeEventListener(CHANGE_THEME_EVENT, this);
   }
 
   /** @internal */
